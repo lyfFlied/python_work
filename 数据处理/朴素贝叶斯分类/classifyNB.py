9a7639a21b77f586c517a767f2fb9978d6c3e6e6
官江涛 <1163236754@qq.com>
2019-04-16 01:26:33 +0800

    修改一下贝叶斯分类器

--- 数据处理/朴素贝叶斯分类/classifyNB.py
+++ 数据处理/朴素贝叶斯分类/classifyNB.py
@@ -7,23 +7,30 @@ import data_handle as dh
 cat_image_list,dog_image_list = dh.load_data()
 pictureClasses=[0]*200+[1]*200
 
+"""
+    朴素贝叶斯适配器训练函数
+"""
 def NBFit(trainMatrix,trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory)/float(numTrainDocs)
-    p0Num = np.zeros(numWords); p1Num = np.zeros(numWords)
+    p0Num = np.zeros(trainMatrix[0].shape); p1Num = np.zeros(trainMatrix[0].shape)
     p0Denom = 0.0; p1Denom = 0.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
             p1Denom += sum(trainMatrix[i])
         else:
-            p0Num += trainMatrix[i]
+            # 矩阵加法这里有点问题
+            arr =  np.array(trainMatrix[i])
+            print(p0Num.shape)
+            print(arr.shape)
+            p0Num += arr
             p0Denom += sum(trainMatrix[i])
     p1Vect = p1Num/p1Denom
     P0Vect = p0Num/p0Denom
     return P0Vect,p1Vect,pAbusive
 
 
-pFVect,pUVect,pCat=NBFit(cat_image_list,pictureClasses)
-print(pFVect,pUVect,pCat)
+P0Vect,p1Vect,pAbusive=NBFit(cat_image_list,pictureClasses)
+print(P0Vect,p1Vect,pAbusive)
